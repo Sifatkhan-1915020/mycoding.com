8757a44b47aa4b8428dc2a0230c2a7decbd06d57
Sifatkhan-1915020 <69406991+Sifatkhan-1915020@users.noreply.github.com>
2021-06-19 07:47:06 +0600

    hola gola

--- array app.cpp
+++ array app.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
 #include <math.h>
-
+#include <string>
+#include <stdio.h>
+#include <stddef.h>
 using namespace std;
 
+ 
 int main()
- {
- 	int array[3][3];
- 	int k=1;
- 	  for(int i=0;i<3;i++)
- 	   {
- 	   	  for(int j=0;j<3;j++)
- 	   	   {
- 	   	   	   array[i][j]=k++;
-			   }
-		}
-		
-		for(int i=0;i<3;i++)
- 	   {
- 	   	  for(int j=0;j<3;j++)
- 	   	   {
- 	   	   	   cout<<array[i][j]<<" ";
-			   }
-		 cout<<endl;
-		}
+ {  
+   int array[]={1,2,3,4,5};
+   
+ // array;
+    cout<<*(&array+1)-array;
  }
